5846c02037ea9886dfc1504e91a27365718c9071
Jose Carlos Barrales Quirino <jose.barrales@bestday.com>
2019-10-21 14:54:53 -0500

    first commit

---
+++ Prometheus.Exporters.MSSQL/Abstractions/IConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prometheus.Exporters.MSSQL.Abstractions
+{
+    public interface IConfig
+    {
+        int ServerPort { get; set; }
+        bool AddExporterMetrics { get; set; }
+        string DataSource { get; set; }
+        string ConfigFile { get; set; }
+        string ServerPath { get; set; }
+        string ConfigUrl { get; set; }
+        string Instance { get; set; }
+        string[] Instances { get; set; }
+        string[] ConnectionString { get; set; }
+    }
+}
---
+++ Prometheus.Exporters.MSSQL/Options/ConfigOptions.cs
@@ -0,0 +1,29 @@
+﻿using Prometheus.Exporters.MSSQL.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prometheus.Exporters.MSSQL.Options
+{
+    public class ConfigOptions : IConfig
+    {
+        public string DataSource { get; set; } 
+
+        public string ConfigFile { get; set; } 
+
+        public string ServerPath { get; set; } = "metrics";
+
+        public int ServerPort { get; set; } = 80;
+
+        public string Instance { get; set; }
+
+        public string[] ConnectionString { get; set; }
+
+        public string[] Instances { get; set; }
+
+        public string ConfigUrl { get; set; } 
+
+        public bool AddExporterMetrics { get; set; } = false;
+    }
+}
